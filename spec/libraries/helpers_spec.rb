fe6ea38ead7287b96cb932406ca378f073580154
Dan Webb <dan.webb@damacus.io>
2018-06-11 09:32:35 +0100

    Fix specs now I've removed the run state

--- spec/libraries/helpers_spec.rb
+++ spec/libraries/helpers_spec.rb
@@ -10,15 +10,14 @@ RSpec.describe PostgresqlCookbook::Helpers do
   describe '#data_dir(version)' do
     before do
       allow(subject).to receive(:[]).with('platform_family').and_return(platform_family)
+      @new_resource = double(version: 9.6)
     end
-
-    let(:pg_version) { '9.6' }
 
     context 'with rhel family and Postgres 9.6' do
       let(:platform_family) { 'rhel' }
 
       it 'returns the correct path' do
-        expect(subject.data_dir(pg_version)).to eq '/var/lib/pgsql/9.6/data'
+        expect(subject.data_dir(@new_resource)).to eq '/var/lib/pgsql/9.6/data'
       end
     end
 
@@ -26,7 +25,7 @@ RSpec.describe PostgresqlCookbook::Helpers do
       let(:platform_family) { 'debian' }
 
       it 'returns the correct path' do
-        expect(subject.data_dir(pg_version)).to eq '/var/lib/postgresql/9.6/main'
+        expect(subject.data_dir(@new_resource)).to eq '/var/lib/postgresql/9.6/main'
       end
     end
   end
@@ -34,15 +33,14 @@ RSpec.describe PostgresqlCookbook::Helpers do
   describe '#conf_dir(version)' do
     before do
       allow(subject).to receive(:[]).with('platform_family').and_return(platform_family)
+      @new_resource = double(version: 9.6)
     end
-
-    let(:pg_version) { '9.6' }
 
     context 'with rhel family and Postgres 9.6' do
       let(:platform_family) { 'rhel' }
 
       it 'returns the correct path' do
-        expect(subject.conf_dir(pg_version)).to eq '/var/lib/pgsql/9.6/data'
+        expect(subject.conf_dir(@new_resource)).to eq '/var/lib/pgsql/9.6/data'
       end
     end
 
@@ -50,7 +48,7 @@ RSpec.describe PostgresqlCookbook::Helpers do
       let(:platform_family) { 'debian' }
 
       it 'returns the correct path' do
-        expect(subject.conf_dir(pg_version)).to eq '/etc/postgresql/9.6/main'
+        expect(subject.conf_dir(@new_resource)).to eq '/etc/postgresql/9.6/main'
       end
     end
   end
@@ -58,15 +56,14 @@ RSpec.describe PostgresqlCookbook::Helpers do
   describe '#platform_service_name(version)' do
     before do
       allow(subject).to receive(:[]).with('platform_family').and_return(platform_family)
+      @new_resource = double(version: 9.6)
     end
-
-    let(:pg_version) { '9.6' }
 
     context 'with rhel family and Postgres 9.6' do
       let(:platform_family) { 'rhel' }
 
       it 'returns the correct service name' do
-        expect(subject.platform_service_name(pg_version)).to eq 'postgresql-9.6'
+        expect(subject.platform_service_name(@new_resource)).to eq 'postgresql-9.6'
       end
     end
 
@@ -74,7 +71,7 @@ RSpec.describe PostgresqlCookbook::Helpers do
       let(:platform_family) { 'debian' }
 
       it 'returns the correct service name' do
-        expect(subject.platform_service_name(pg_version)).to eq 'postgresql'
+        expect(subject.platform_service_name(@new_resource)).to eq 'postgresql'
       end
     end
   end
