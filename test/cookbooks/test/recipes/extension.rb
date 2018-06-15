114814ec89125360c0dba090a7a495453cca29af
Dan Webb <dan.webb@damacus.io>
2018-06-15 08:10:56 +0100

    Tidy up spec slightly

--- test/cookbooks/test/recipes/extension.rb
+++ test/cookbooks/test/recipes/extension.rb
@@ -1,9 +1,17 @@
 postgresql_repository 'install'
 
-postgresql_server_install 'package'
+postgresql_server_install 'package' do
+  password '12345'
+  port 5432
+  setup_repo true
+  action [:install, :create]
+end
 
 postgresql_database 'test_1'
 
-postgresql_extension 'openfts' do
+package 'postgresql-contrib'
+
+postgresql_extension 'adminpack' do
+  source_directory '/usr/share/pgsql/extension/'
   database 'test_1'
 end
